adc5b64c7829220424aa3b701edff063a150ef2a
rahmawanilham <88438264+rahmawanilham@users.noreply.github.com>
2022-10-24 16:13:42 +0700

    Add files via upload

--- datamhs.sql
+++ datamhs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2022 at 12:26 AM
+-- Generation Time: Oct 24, 2022 at 10:07 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -46,49 +46,82 @@ INSERT INTO `akun` (`id`, `username`, `password`, `level`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `alamat`
+--
+
+CREATE TABLE `alamat` (
+  `id_alamat` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `rt` varchar(5) NOT NULL,
+  `rw` varchar(5) NOT NULL,
+  `dusun` varchar(30) NOT NULL,
+  `desa` varchar(50) NOT NULL,
+  `provinsi` varchar(50) NOT NULL,
+  `kota` varchar(50) NOT NULL,
+  `kecamatan` varchar(50) NOT NULL,
+  `negara` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `biodata`
 --
 
 CREATE TABLE `biodata` (
-  `id` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `jenis_kelamin` varchar(30) NOT NULL,
   `agama` varchar(30) NOT NULL,
   `no_tlp` varchar(12) NOT NULL,
   `email` varchar(100) NOT NULL,
   `no_ktp` varchar(30) NOT NULL,
-  `no_kk` varchar(30) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
-  `rt` varchar(10) NOT NULL,
-  `rw` varchar(10) NOT NULL,
-  `dusun` varchar(50) NOT NULL,
-  `desa` varchar(50) NOT NULL,
-  `provinsi` varchar(20) NOT NULL,
-  `kota` varchar(20) NOT NULL,
-  `kecamatan` varchar(20) NOT NULL,
-  `negara` varchar(20) NOT NULL,
-  `nama_ayah` varchar(100) NOT NULL,
-  `tgl_ayah` varchar(50) NOT NULL,
-  `pendidikan_ayah` varchar(50) NOT NULL,
-  `pekerjaan_ayah` varchar(50) NOT NULL,
-  `notlp_ayah` varchar(12) NOT NULL,
-  `nama_ibu` varchar(50) NOT NULL,
-  `tgl_ibu` varchar(50) NOT NULL,
-  `pendidikan_ibu` varchar(50) NOT NULL,
-  `pekerjaan_ibu` varchar(50) NOT NULL,
-  `notlp_ibu` varchar(12) NOT NULL,
-  `nisn` varchar(100) NOT NULL,
-  `slta` varchar(100) NOT NULL,
-  `alamat_slta` varchar(50) NOT NULL,
-  `notlp_slta` varchar(50) NOT NULL
+  `no_kk` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `biodata`
 --
 
-INSERT INTO `biodata` (`id`, `nama`, `jenis_kelamin`, `agama`, `no_tlp`, `email`, `no_ktp`, `no_kk`, `alamat`, `rt`, `rw`, `dusun`, `desa`, `provinsi`, `kota`, `kecamatan`, `negara`, `nama_ayah`, `tgl_ayah`, `pendidikan_ayah`, `pekerjaan_ayah`, `notlp_ayah`, `nama_ibu`, `tgl_ibu`, `pendidikan_ibu`, `pekerjaan_ibu`, `notlp_ibu`, `nisn`, `slta`, `alamat_slta`, `notlp_slta`) VALUES
-(100, 'rahmawan ilham pratama', 'Laki-laki', 'islam', '', '', '', '', '', '', '', '', '', 'jatim', 'madiun', 'sawahan', 'indonesia', '', '', 'sarjana', '', '', '', '', 'sarjana', '', '', '', '', '', '');
+INSERT INTO `biodata` (`nim`, `nama`, `jenis_kelamin`, `agama`, `no_tlp`, `email`, `no_ktp`, `no_kk`) VALUES
+('2005102006', 'Rahmawan Ilham Pratama ilham', 'Laki-laki', 'islam', '08510194818', 'rahmawanemail@gmail.com', '247257251111111', '13513561');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keluarga`
+--
+
+CREATE TABLE `keluarga` (
+  `id_keluarga` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `nama_ayah` varchar(50) NOT NULL,
+  `tgl_ayah` date NOT NULL,
+  `pendidikan_ayah` varchar(50) NOT NULL,
+  `pekerjaan_ayah` varchar(50) NOT NULL,
+  `notlp_ayah` varchar(15) NOT NULL,
+  `nama_ibu` varchar(50) NOT NULL,
+  `tgl_ibu` date NOT NULL,
+  `pendidikan_ibu` varchar(50) NOT NULL,
+  `pekerjaan_ibu` varchar(50) NOT NULL,
+  `notlp_ibu` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sekolah`
+--
+
+CREATE TABLE `sekolah` (
+  `id_sekolah` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `nisn` varchar(30) NOT NULL,
+  `slta` varchar(50) NOT NULL,
+  `alamat_slta` varchar(50) NOT NULL,
+  `notlp_slta` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -101,10 +134,31 @@ ALTER TABLE `akun`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `alamat`
+--
+ALTER TABLE `alamat`
+  ADD PRIMARY KEY (`id_alamat`),
+  ADD KEY `nim` (`nim`);
+
+--
 -- Indexes for table `biodata`
 --
 ALTER TABLE `biodata`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `keluarga`
+--
+ALTER TABLE `keluarga`
+  ADD PRIMARY KEY (`id_keluarga`),
+  ADD KEY `nim` (`nim`);
+
+--
+-- Indexes for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  ADD PRIMARY KEY (`id_sekolah`),
+  ADD KEY `nim` (`nim`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -117,10 +171,44 @@ ALTER TABLE `akun`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `biodata`
+-- AUTO_INCREMENT for table `alamat`
 --
-ALTER TABLE `biodata`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
+ALTER TABLE `alamat`
+  MODIFY `id_alamat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `keluarga`
+--
+ALTER TABLE `keluarga`
+  MODIFY `id_keluarga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  MODIFY `id_sekolah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `alamat`
+--
+ALTER TABLE `alamat`
+  ADD CONSTRAINT `alamat_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `biodata` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `keluarga`
+--
+ALTER TABLE `keluarga`
+  ADD CONSTRAINT `keluarga_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `biodata` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  ADD CONSTRAINT `sekolah_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `biodata` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
